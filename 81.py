07efe0f10b2ba18491b19f8a28acfa856cff16ac
LiuXPeng <LiuXPeng@users.noreply.github.com>
2018-06-28 19:02:43 +0800

    20180628

---
+++ 81.py
@@ -0,0 +1,30 @@
+class Solution:
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: bool
+        """
+        if len(nums) == 0:
+            return False
+        low = 0
+        high = len(nums) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if nums[mid] == target:
+                return True
+            while nums[low] == nums[mid] and low < mid:
+                low += 1
+            while nums[high] == nums[mid] and mid < high:
+                high -= 1
+            if nums[low] <= nums[mid]:
+                if nums[low] <= target <= nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:
+                if nums[mid] <= target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+        return False
